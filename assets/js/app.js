e6c42d65f3d6faecbaa4c686bae1eb3a6744c884
RustySnek <jodlowskipascal@gmail.com>
2023-10-04 10:06:52 +0200

    :heavy_plus_sign: Add SortableJs Dep

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,15 +18,33 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Sortable from "../vendor/sortable"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let Hooks = {}
+Hooks.Sortable = {
+  mounted() {
+    let sorter = new Sortable(this.el, {
+      group: this.el.dataset.group,
+      animation: 150,
+      delay: 0,
+      dragClass: "drag-item",
+      ghostClass: "drag-ghost",
+      forceFallback: true,
+      onEnd: e => {
+        let params = { old: e.oldIndex, new: e.newIndex, to: e.to.dataset, from: e.from.dataset, ...e.item.dataset }
+        this.pushEventTo(this.el, "reposition", params)
+      }
+    })
+  }
+}
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
