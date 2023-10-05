b139cf8ebe35a5194813c213d0ab70debc90f6e8
RustySnek <jodlowskipascal@gmail.com>
2023-10-05 23:10:45 +0200

    :fire: Remove unnecessary code

--- lib/drag_board/board.ex
+++ lib/drag_board/board.ex
@@ -16,29 +16,3 @@ defmodule DragBoard.Board do
     |> validate_required([:name, :group])
   end
 end
-
-defmodule DragBoard.Boards do
-  alias DragBoard.{Board, Repo}
-  import Ecto.Query, only: [from: 2]
-
-  def list_boards() do
-    from(board in DragBoard.Board, select: board)
-    |> DragBoard.Repo.all()
-    |> DragBoard.Repo.preload(
-      board_tasks: from(t in DragBoard.BoardTask, order_by: [asc: t.position])
-    )
-  end
-
-  def add_board(name, group) do
-    changeset =
-      %Board{}
-      |> Board.changeset(%{name: name, group: group})
-
-    case Repo.insert(changeset) do
-      {:ok, _board} -> {:ok, "Board added successfully"}
-      {:error, changeset} -> {:error, "Failed to add board", changeset}
-    end
-
-    list_boards()
-  end
-end
